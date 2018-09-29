c5ed81d6f5a2da3ab0ed428a4f1e082294107cea
Parrot023 <Andreas081203@gmail.com>
2018-09-29 13:48:09 +0200

    added basic highscore object

--- Database-project.py
+++ Database-project.py
@@ -1,1 +1,11 @@
 #Database administration program
+class Highscore(object):
+
+    def __init__(self, game, score):
+
+        self.game = game
+        self.score = score
+
+a = Highscore("Snake", 20)
+
+print(a.game)
