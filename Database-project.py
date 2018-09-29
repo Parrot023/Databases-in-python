2f096549b665933d4857b28f772d5aca05c1b64d
Parrot023 <Andreas081203@gmail.com>
2018-09-29 16:55:16 +0200

    Added function to create new highscore and added comments

--- Database-project.py
+++ Database-project.py
@@ -1,11 +1,33 @@
 #Database administration program
+
+#Highscore object
 class Highscore(object):
 
-    def __init__(self, game, score):
+    def __init__(self, _game, _score, _player_name):
 
-        self.game = game
-        self.score = score
+        #defining variables needed by this object
+        self.game = _game
+        self.score = _score
+        self.player_name = _player_name
 
-a = Highscore("Snake", 20)
 
-print(a.game)
+#This function takes input from the user an stores it in the 3 variables defined at the top
+def New_Highscore():
+
+    #global is needed to tell python that we are workin with global variables
+    global game, score, player_name, a
+
+    #raw_input is a way to take text format inputs from the user.
+    #with the normal input function you only get integers
+    game = raw_input("Which game do you want to set a highscore for? ")
+    score = input("How many points did the player get? ")
+    player_name = raw_input("what is the player called? ")
+
+    #Creates a new highscore object
+    a = Highscore(game, score, player_name)
+
+#runs function New_Highscore
+New_Highscore()
+
+#prints the name of the player who entered his or her highscore
+print("the player who wrote their score is called " + a.player_name)
